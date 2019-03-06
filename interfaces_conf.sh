af830c18a8d3a4e41e14b247aed5d69755ddf948
Seb-Web <48294454+Seb-Web@users.noreply.github.com>
2019-03-06 23:55:44 +0100

    Update interfaces_conf.sh

--- interfaces_conf.sh
+++ interfaces_conf.sh
@@ -3,12 +3,15 @@
 # Récupération du répertoire d'éxecution du script
 rep_firewall=$(dirname $(readlink -f $0))
 
+# Récupération des information sur les interfaces réseau detecté par le système
 for iface in `ls /sys/class/net`
     do
         if [ -d "/sys/class/net/${iface}/device" ]
         then
-            cat /sys/class/net/${iface}/address
-            udevadm info "/sys/class/net/${iface}/device/driver"/* | grep -E "ID_MODEL_FROM_DATABASE|ID_VENDOR_FROM_DATABASE" | cut -d "=" -f2
+            iface_mac=`cat /sys/class/net/${iface}/address`
+            iface_info=`udevadm info "/sys/class/net/${iface}/device/driver"/* | grep -E "ID_MODEL_FROM_DATABASE|ID_VENDOR_FROM_DATABASE" | cut -d "=" -f2`
+            echo -e "${iface_mac}"
+            echo -e "${iface_info}"
             echo
         fi
     done
