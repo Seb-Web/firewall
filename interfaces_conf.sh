d3ecc2da2e47f86ea223bc11a441726b5ecc7273
Seb-Web <48294454+Seb-Web@users.noreply.github.com>
2019-03-09 14:35:22 +0100

    Update interfaces_conf.sh

--- interfaces_conf.sh
+++ interfaces_conf.sh
@@ -2,38 +2,60 @@
 extention=".dev"
 # Déclaration des variables
 # déclaration de l'architecture firewall
-declare -A interface_couleur=( ["red"]="\033[31m" ["green"]="\033[32m" ["orange"]="\033[33m" )
 
-declare -A interface_iface
-declare -A interface_address
-declare -A interface_network
-declare -A interface_netmask
-declare -A interface_gateway
-declare -A interface_dns
-declare -A interface_mac
-declare -A interface_model
-declare -A interface_vendor
+declare -A iface_name
+declare -A iface_color=( ["red"]="\033[31m" ["green"]="\033[32m" ["orange"]="\033[33m" )
+declare -A iface_inet=( ["red"]="dhcp" ["green"]="static" ["orange"]="static" )
+declare -A iface_address
+declare -A iface_network
+declare -A iface_netmask
+declare -A iface_gateway
+declare -A iface_dns
+declare -A iface_mac
+declare -A iface_model
+declare -A iface_vendor
 
 # Déclaration des fonction
-interface_dispo()
+function iface_dispo()
 {
-    echo -e "le système a détecté ${#interface_mac[@]} carte réseau"
+    echo -e "le système a détecté ${#iface_mac[@]} carte réseau"
     echo
     for (( i=1;i<$count; i++))
         do
-        if [ "${interface_choix["${i}"]}" == "" ]
+        if [ "${iface_choix["${i}"]}" == "" ]
             then
             echo -e "#### Choix n° ${i}"
-            echo -e "# ${interface_mac[${i}]}"
-            echo -e "# ${interface_model[${i}]}"
-            echo -e "# ${interface_vendor[${i}]}"
+            echo -e "# ${iface_mac[${i}]}"
+            echo -e "# ${iface_model[${i}]}"
+            echo -e "# ${iface_vendor[${i}]}"
             echo -e "####"
             echo
         else
-            echo -e "#### Choix n° ${i} ## ${interface_couleur[${interface_choix[${i}]}]}${interface_choix[${i}]}\033[0m ## ${interface_mac[${i}]} ## ${interface_network[${i}]}/${interface_netmask[${i}]} ## ${interface_address[${i}]}"
+            echo -e "#### Choix n° ${i} ## ${iface_color[${iface_choix[${i}]}]}${iface_choix[${i}]}\033[0m ## ${iface_mac[${i}]} ## ${iface_network[${i}]}/${iface_netmask[${i}]} ## ${iface_address[${i}]}"
             echo
         fi
     done
+}
+
+function saisie()
+{
+    local __in1=$1
+    local __in2=$2
+
+    verif="0"
+
+    while [ "$verif" == "0" ]
+        do
+        echo -n -e "${__in1}"
+        read clavier
+        if [ "$clavier" == "" ]
+            then
+            echo "Erreur de saisie !""!"
+            else
+            verif="1"
+            fi
+        done
+        eval $__in2="'$clavier'"
 }
 # Récupération du répertoire d'éxecution du script
 rep_firewall=$(dirname $(readlink -f $0))
@@ -44,62 +66,85 @@ for iface in `ls /sys/class/net`
     do
     if [ -d "/sys/class/net/${iface}/device" ]
         then
-        interface_mac[${count}]=`cat /sys/class/net/${iface}/address`
+        iface_mac[${count}]=`cat /sys/class/net/${iface}/address`
         while read line
             do
             if [[ $line =~ ^"E: ID_MODEL_FROM_DATABASE" ]]
                 then
-                interface_model[${count}]=`echo ${line} | grep -E "ID_MODEL_FROM_DATABASE" | cut -d "=" -f2`
+                iface_model[${count}]=`echo ${line} | grep -E "ID_MODEL_FROM_DATABASE" | cut -d "=" -f2`
                 fi
             if [[ $line =~ ^"E: ID_VENDOR_FROM_DATABASE" ]]
                 then
-                interface_vendor[${count}]=`echo ${line} | grep -E "ID_VENDOR_FROM_DATABASE" | cut -d "=" -f2`
+                iface_vendor[${count}]=`echo ${line} | grep -E "ID_VENDOR_FROM_DATABASE" | cut -d "=" -f2`
                 fi
             done < <(udevadm info "/sys/class/net/${iface}/device/driver"/*)
         count=$(($count+1))
         fi
     done
 
-
 # Paramétrage des cartes réseaux
 confirm1="non"
 while [ ! "${confirm1}" == "oui" ]
     do
     clear
-    unset interface_choix
-    declare -A interface_choix
+    unset iface_choix
+    declare -A iface_choix
 
-    for couleur in "${!interface_couleur[@]}"
+    for couleur in "${!iface_color[@]}"
         do
         confirm2="non"
         while [ ! "${confirm2}" == "oui" ]
             do
-            interface_dispo
-            echo -n -e "Quel est votre choix pour l'interface ${interface_couleur[${couleur}]}${couleur}\033[0m ? "
-            read interface_choix["${couleur}"]
-            interface_choix["${interface_choix["${couleur}"]}"]="${couleur}" 
-            echo -n "réseau : "    
-            read interface_network["${interface_choix["${couleur}"]}"]
-            echo -n "masque réseau : "    
-            read interface_netmask["${interface_choix["${couleur}"]}"]
-            echo -n "adresse : "    
-            read interface_address["${interface_choix["${couleur}"]}"]
-            echo -n "passerelle : "
-            read interface_gateway["${interface_choix["${couleur}"]}"]
-            echo -n "DNS : "
-            read interface_dns["${interface_choix["${couleur}"]}"]
+            iface_dispo
+            # on boucle sur la demande de choix, tant que l'on ne choisi pas une carte non configurer
+            while [ true ]
+                do            
+                saisie "Quel est votre choix pour l'interface ${iface_color[${couleur}]}${couleur}\033[0m ? " 'iface_choix["${couleur}"]'
+                if [ "${iface_choix["${iface_choix["${couleur}"]}"]}" == "" ] 
+                    then 
+                    iface_choix["${iface_choix["${couleur}"]}"]="${couleur}"
+                    break
+                    else
+                    echo "Ce choix à déjà été parametré."
+                    fi
+                done
+            if [ $couleur == "red" ]
+                then
+                    while [ true ]
+                    do
+                        saisie 'dhcp ou static : ' 'iface_inet["${couleur}"]'
+                        if [ "${iface_inet["${couleur}"]}" == "static" ]
+                            then 
+                            iface_inet["${couleur}"]="static"
+                            break
+                            elif [ "${iface_inet["${couleur}"]}" == "dhcp" ]
+                            then 
+                            iface_inet["${couleur}"]="dhcp"
+                            break
+                            fi
+                        echo "erreur de saisie !""!"
+                    done
+                fi
+            if [ "${iface_inet[${couleur}]}" == "static" ]
+                then
+                saisie 'réseau : ' 'iface_network["${iface_choix["${couleur}"]}"]'
+                saisie 'masque réseau : ' 'iface_netmask["${iface_choix["${couleur}"]}"]'
+                saisie 'adresse : ' 'iface_address["${iface_choix["${couleur}"]}"]'
+                saisie 'passerelle : ' 'iface_gateway["${iface_choix["${couleur}"]}"]'
+                saisie 'DNS : ' 'iface_dns["${iface_choix["${couleur}"]}"]'
+                fi
             echo
             echo -n "Confirmer (NON/oui) ? "
             read confirm2
             if [ ! "${confirm2}" == "oui" ]
             then
-                unset interface_choix["${interface_choix["${couleur}"]}"]
-                unset interface_choix["${couleur}"]
+                unset iface_choix["${iface_choix["${couleur}"]}"]
+                unset iface_choix["${couleur}"]
             fi
             clear
             done
         done
-    interface_dispo
+    iface_dispo
     echo
     echo -n "Confirmer (NON/oui) ? "
     read confirm1
@@ -107,9 +152,9 @@ while [ ! "${confirm1}" == "oui" ]
 
 # écriture des règles d'attribution de nom pour les interfaces
 echo > "${rep_firewall}/config/udev/rules.d/75-firewall-persistant-net.rules${extention}"
-for i in "${!interface_choix[@]}"
+for i in "${!iface_choix[@]}"
     do 
-    echo -e "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${interface_mac[${interface_choix["$i"]}]}\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"$i\"" >> "${rep_firewall}/config/udev/rules.d/75-firewall-persistant-net.rules${extention}"
+    echo -e "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${iface_mac[${iface_choix["$i"]}]}\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"$i\"" >> "${rep_firewall}/config/udev/rules.d/75-firewall-persistant-net.rules${extention}"
 
     done
 ln -sf "${rep_firewall}/config/udev/rules.d/75-firewall-persistant-net.rules${extention}" "/etc/udev/rules.d/75-firewall-persistant-net.rules${extention}"
