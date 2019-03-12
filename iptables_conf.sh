0172b00862c92b9cd6889d88935d96b114e7d9ac
Seb-Web <48294454+Seb-Web@users.noreply.github.com>
2019-03-12 16:50:21 +0100

    Update iptables_conf.sh

--- iptables_conf.sh
+++ iptables_conf.sh
@@ -3,6 +3,10 @@
 # Récupération du répertoire d'éxecution du script
 rep_firewall=$(dirname $(readlink -f $0))
 
+#Chargement de l'entete de présentation
+source "${rep_firewall}/config/entete"
+read -s -n1 -p "Appuyez sur une touche pour continuer..."; echo
+
 ## chargement des module
 modprobe ip_conntrack_ftp
 modprobe ip_nat_ftp
@@ -22,10 +26,15 @@ iptables -t mangle -X
 iptables -t nat -F POSTROUTING
 
 ####initialisation des variables
+port_ssh=60022
 # interfaces
 source "${rep_firewall}/config/interfaces"
 # port routage
 source "${rep_firewall}/config/ports_routage"
+
+## Autoriser le trafic local
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 ##Préréglage en mode fortress
 iptables -P INPUT   DROP
@@ -53,10 +62,11 @@ echo "#do not allow ACK pkts to create new connection (normal behavior SYN->, <-
 echo 0 > /proc/sys/net/netfilter/nf_conntrack_tcp_loose
 echo "#enable TCP timestamps as SYN cookies utilize this TCP"
 echo 1 > /proc/sys/net/ipv4/tcp_timestamps
+echo
+echo "Quelque Tweaks"
 echo "#Conntrack Entry Tuning (Calculate your own values ! depending on your hardware)"
 echo 200000 > /proc/sys/net/netfilter/nf_conntrack_max
-
-echo 500000 > /sys/module/nf_conntrack/parameters/hashsize 
+echo 500000 > /sys/module/nf_conntrack/parameters/hashsize
 sysctl -p
 
 ## anti DOS
@@ -64,59 +74,50 @@ iptables -A INPUT -m state --state INVALID -j DROP
 
 
 ##Pour permettre à une connexion déjà ouverte de recevoir du trafic
-iptables -A INPUT  -i "${red_iface}"    -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -o "${red_iface}"    -j ACCEPT
-iptables -A INPUT  -i "${green_iface}"  -j ACCEPT
-iptables -A OUTPUT -o "${green_iface}"  -j ACCEPT
-iptables -A INPUT  -i "${orange_iface}" -j ACCEPT
-iptables -A OUTPUT -o "${orange_iface}" -j ACCEPT
-
-
-## Autoriser le trafic local
-iptables -A INPUT  -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT  -i "${int_iface}"    -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o "${int_iface}"    -j ACCEPT
+iptables -A INPUT  -i "${zone01_iface}"  -j ACCEPT
+iptables -A OUTPUT -o "${zone01_iface}"  -j ACCEPT
+iptables -A INPUT  -i "${zone02_iface}" -j ACCEPT
+iptables -A OUTPUT -o "${zone02_iface}" -j ACCEPT
 
 ## On autorise les ports necessaires a notre configuration serveur :
-iptables -A INPUT  -i "${red_iface}"    -p tcp --dport 60022 -j ACCEPT
-#iptables -A INPUT  -i "${red_iface}"    -p tcp --dport 60443 -j ACCEPT
+iptables -A INPUT  -i "${int_iface}"    -p tcp --dport ${port_ssh} -j ACCEPT
+#iptables -A INPUT  -i "${int_iface}"    -p tcp --dport 60443 -j ACCEPT
 
-## On autorise les pings entrants sur red
-iptables -A INPUT -i "${red_iface}"    -p icmp -j ACCEPT
-
-iptables -A OUTPUT -p icmp -j ACCEPT
-
-## autorisation de forward red<-->orange
-iptables -A FORWARD -o "${red_iface}"    -i "${orange_iface}" -j ACCEPT
-iptables -A FORWARD -o "${orange_iface}" -i "${red_iface}"    -j ACCEPT
-## autorisation de forward red<-->green
-iptables -A FORWARD -o "${red_iface}"    -i "${green_iface}"  -j ACCEPT
-iptables -A FORWARD -o "${green_iface}"  -i "${red_iface}"    -m state --state ESTABLISHED,RELATED -j ACCEPT
-## autorisation de forward green<-->orange
-iptables -A FORWARD -o "${green_iface}"  -i "${orange_iface}" -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -o "${orange_iface}" -i "${green_iface}"  -j ACCEPT
-
-
+## autorisation de forward int<-->zone01 pour les liens établies, cette zone est une zone de travail
+iptables -A FORWARD -i "${zone01_iface}" -o "${int_iface}" -j ACCEPT
+iptables -A FORWARD -i "${int_iface}"    -o "${zone01_iface}" -m state --state ESTABLISHED,RELATED -j ACCEPT
+## autorisation de forward int<-->zone02 pour les liens établies, cette zone est une dmz
+iptables -A FORWARD -i "${zone02_iface}" -o "${int_iface}" -j ACCEPT
+iptables -A FORWARD -i "${int_iface}"    -o "${zone02_iface}" -j ACCEPT
+## autorisation de forward zone01-->zone02, pour les liens établies
+# tout ce qui vient de la zone01 en direction de la zone02 est accepter
+iptables -A FORWARD -i "${zone01_iface}" -o "${zone02_iface}" -j ACCEPT
+iptables -A FORWARD -i "${zone02_iface}" -o "${zone01_iface}" -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 ## Mise en place du masquerade
-iptables -t nat -A POSTROUTING -o "${red_iface}" ! -d "${red_ip}" -s "${orange_network}" -j MASQUERADE
-iptables -t nat -A POSTROUTING -o "${red_iface}" ! -d "${red_ip}" -s "${green_network}"  -j MASQUERADE
+# a l'exeption de tout ce qui est a destination de l'ip internet
+# ce qui permet un rebouclage sur les service interne avec un DNS externe
+iptables -t nat -A POSTROUTING -o "${int_iface}" ! -d "${int_ip}" -s "${zone01_network}" -j MASQUERADE
+iptables -t nat -A POSTROUTING -o "${int_iface}" ! -d "${int_ip}" -s "${zone02_network}" -j MASQUERADE
 
 # Mise en place du routage de port
 for i in "${!port_routage[@]}"
 	do
 	service_name=`echo "${port_routage[$i]}" | cut -d "|" -f1`
 	proto=`echo "${port_routage[$i]}" | cut -d "|" -f2`
-	red_port=`echo "${port_routage[$i]}" | cut -d "|" -f3`
+	int_port=`echo "${port_routage[$i]}" | cut -d "|" -f3`
 	ip_destination=`echo "${port_routage[$i]}" | cut -d "|" -f4`
 	port_destination=`echo "${port_routage[$i]}" | cut -d "|" -f5`
     echo "${service_name}"
-    echo "iptables -t nat -A PREROUTING -i \"${red_iface}\" -p \"${proto}\" --dport \"${red_port}\" -j DNAT --to-destination \"${ip_destination}:${port_destination}\""
-    iptables -t nat -A PREROUTING -i "${red_iface}" -p "${proto}" --dport "${red_port}" -j DNAT --to-destination "${ip_destination}:${port_destination}"
+    echo "iptables -t nat -A PREROUTING -i \"${int_iface}\" -p \"${proto}\" --dport \"${int_port}\" -j DNAT --to-destination \"${ip_destination}:${port_destination}\""
+    iptables -t nat -A PREROUTING -i "${int_iface}" -p "${proto}" --dport "${int_port}" -j DNAT --to-destination "${ip_destination}:${port_destination}"
 	done
 
 ## on bloque tout le reste
-iptables -A INPUT -i "${red_iface}"    -j REJECT
-iptables -A INPUT -i "${orange_iface}" -j REJECT
+iptables -A INPUT -i "${int_iface}"    -j REJECT
+iptables -A INPUT -i "${zone02_iface}" -j REJECT
 
-iptables -nvL
-iptables -L -t nat
+iptables -nvL --line-number
+iptables -L -t nat --line-number
