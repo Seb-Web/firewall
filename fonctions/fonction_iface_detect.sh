2d96b103c2884e85239265c84a201ba168a626e1
Seb-Web <seb.web.1975@gmail.com>
2019-03-16 08:07:07 +0100

    Redéfinition de variable
    Modification de la portée de certaine variable
    Rectification de placement de variable "__count"

--- fonctions/fonction_iface_detect.sh
+++ fonctions/fonction_iface_detect.sh
@@ -1,33 +1,35 @@
 function iface_detect()
 {
-    # Cette fonction doit avoir 3 valeurs d'entrées identifiant les tableu a remplir
+    # Cette fonction doit avoir 3 parametres d'entrées identifiant les tableu a remplir
     # exemple iface_detect "iface_mac" "iface_model" "iface_vendor"
     # le resultat sera stocké dans les tableaux indexés iface_mac[@] iface_model[@] et iface_vendor[@]
     local __tab_mac=$1
     local __tab_model=$2
     local __tab_vendor=$3
     local __tmp
+    local __count=0
     # Cette methode permet de récupérer la MAC, LE MODEL et LE VENDOR
-    count=1
+
     for iface in `ls /sys/class/net`
     do
         if [ -d "/sys/class/net/${iface}/device" ]
         then
-            eval "${__tab_mac}[${count}]"="$(cat /sys/class/net/${iface}/address)"
+            __count=$((${__count}+1))
+            eval "${__tab_mac}[${__count}]"="$(cat /sys/class/net/${iface}/address)"
             while read line
             do
                 if [[ $line =~ ^"E: ID_MODEL_FROM_DATABASE" ]]
                 then
                     __tmp=$(echo ${line} | grep -E "ID_MODEL_FROM_DATABASE" | cut -d"=" -f2)
-                    eval ${__tab_model}[${count}]='"${__tmp}"'
+                    eval ${__tab_model}[${__count}]='"${__tmp}"'
                 fi
                 if [[ $line =~ ^"E: ID_VENDOR_FROM_DATABASE" ]]
                 then
                     __tmp=$(echo ${line} | grep -E "ID_VENDOR_FROM_DATABASE" | cut -d"=" -f2)
-                    eval ${__tab_vendor}[${count}]='"${__tmp}"'
+                    eval ${__tab_vendor}[${__count}]='"${__tmp}"'
                 fi
             done < <(udevadm info "/sys/class/net/${iface}/device/driver"/*)
-            count=$(($count+1))
         fi
     done
+    eval "iface_count"="'${__count}'"
 }
