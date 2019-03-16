3f9bcf34c5e249bbd6f6b335e57307f5f094da89
Seb-Web <seb.web.1975@gmail.com>
2019-03-16 08:10:31 +0100

    Redefinition de portée de variable
    Redefinition de portée de variable pour eviter le conflit avec d'autre
    fonctions

--- fonctions/fonction_saisie.sh
+++ fonctions/fonction_saisie.sh
@@ -1,20 +1,23 @@
 function saisie()
 {
+    # la fonction à besoin de 3 parametres d'entré "Texte a affiché" "Variable de sortie" "regex de test"
+    # exemple: saisie "Confimez votre choix: " "var_conf" "^oui$"
+
     local __texte=$1
     local __variable=$2
     local __regex=$3
+    local __verif="0"
 
-    verif="0"
-    while [ "$verif" == "0" ]
-        do
+    while [ "${__verif}" == "0" ]
+    do
         echo -n -e "${__texte}"
         read __saisie
         if [[ "${__saisie}" =~ $__regex ]]
-            then
-            verif="1"
-            else
+        then
+            __verif="1"
+        else
             echo "Erreur de saisie !""!"
-            fi
-        done
-        eval $__variable="'${__saisie}'"
+        fi
+    done
+    eval $__variable="'${__saisie}'"
 }
