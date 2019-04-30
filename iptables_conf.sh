c2e2a68b0308b1bcbd4f5602890eb9a9e31b6580
Seb-Web <48294454+Seb-Web@users.noreply.github.com>
2019-04-30 13:04:24 +0200

    Add files via upload

--- iptables_conf.sh
+++ iptables_conf.sh
@@ -4,9 +4,11 @@
 rep_exec=$(dirname $(readlink -f $0))
 rep_fonctions="${rep_exec}/fonctions"
 rep_config="${rep_exec}/config"
+
 # Chargement des fonctions
 source "${rep_fonctions}/fonction_pause.sh"
 clear
+
 #Chargement de l'entete de présentation
 source "${rep_exec}/entetes/entete_iptables_conf"
 pause
@@ -32,18 +34,21 @@ iptables -t nat -F POSTROUTING
 
 # interfaces
 source "${rep_config}/zones_def.conf"
-source "${rep_config}/zones_def.dev"
+
+iface_address[${zone_iface["wan1"]}]="`ip a show "${iface_name[${zone_iface["wan1"]}]}" | grep "inet " | sed -E "s/ +/ /g" | cut -d" " -f3 | cut -d"/" -f1`"
 # port routage
 source "${rep_config}/ports_routage.conf"
+
 # acces externe
 source "${rep_config}/acces_externe.conf"
 
 ## Autoriser le trafic local
 iptables -A INPUT  -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
+
 ##### ne pas perdre la main pendant la phase de dev 
-iptables -A INPUT  -i zone1 -s 192.168.200.0/24 -j ACCEPT
-iptables -A OUTPUT -o zone1 -d 192.168.200.0/24 -j ACCEPT
+iptables -A INPUT  -i "${iface_name[${zone_iface["lan1"]}]}" -s 192.168.200.20 -j ACCEPT
+iptables -A OUTPUT -o "${iface_name[${zone_iface["lan1"]}]}" -d 192.168.200.20 -j ACCEPT
 ##### /ne pas perdre la main pendant la phase de dev 
 
 ##Préréglage en mode fortress
@@ -51,56 +56,32 @@ iptables -P INPUT   DROP
 iptables -P OUTPUT  DROP
 iptables -P FORWARD DROP
 
-echo "# Activation du forwading"
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo "# Enable Spoof protection (reverse-path filter) Turn on Source Address Verification in all interfaces to prevent some spoofing attacks"
-echo 1 > /proc/sys/net/ipv4/conf/default/rp_filter
-echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
-echo "# Enable SYN Cookie"
-echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-echo "# Do not accept ICMP redirects (prevent some MITM attacks)"
-echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
-echo 0 > /proc/sys/net/ipv6/conf/all/accept_redirects
-echo "# Accept ICMP redirects only for gateways listed in our default gateway list (enabled by default)"
-echo 1 > /proc/sys/net/ipv4/conf/all/secure_redirects
-echo "# Do not send ICMP redirects (we are not a router)"
-echo 0 > /proc/sys/net/ipv4/conf/all/send_redirects
-echo "# Do not accept IP source route packets (we are not a router)"
-echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
-echo 0 > /proc/sys/net/ipv6/conf/all/accept_source_route
-echo "#do not allow ACK pkts to create new connection (normal behavior SYN->, <-SYN,ACK, ACK->)"
-echo 0 > /proc/sys/net/netfilter/nf_conntrack_tcp_loose
-echo "#enable TCP timestamps as SYN cookies utilize this TCP"
-echo 1 > /proc/sys/net/ipv4/tcp_timestamps
-echo
-echo "Quelque Tweaks"
-echo "#Conntrack Entry Tuning (Calculate your own values ! depending on your hardware)"
-echo 200000 > /proc/sys/net/netfilter/nf_conntrack_max
-echo 500000 > /sys/module/nf_conntrack/parameters/hashsize
+## Réglages kernel
+source "${rep_config}/kernel_net.conf"
 sysctl -p
 
 ## anti DOS
 iptables -A INPUT -m state --state INVALID -j DROP
 
 ## Blocage de l'icmp autre que pour la zone1
-#iptables -A INPUT -p icmp ! -s {$zone1_network} -j DROP
+iptables -A INPUT -i int1 -p icmp -s 0.0.0.0/0 -j DROP
 
 ##Pour permettre à une connexion déjà ouverte de recevoir du trafic
 echo "####"
-for iface_idx in "${!iface_name[@]}"
+for zone_name in "${!zone_iface[@]}"
 do
-    if [[ "${iface_name[${iface_idx}]}" =~ ^zone ]]
+    if [[ "${iface_name[${zone_iface[${zone_name}]}]}" =~ ^lan ]]
     then
-        echo "iptables -A INPUT  -i "${iface_name[${iface_idx}]}" -j ACCEPT"
-        iptables -A INPUT  -i "${iface_name[${iface_idx}]}" -j ACCEPT
-        echo "iptables -A OUTPUT -o "${iface_name[${iface_idx}]}" -j ACCEPT"
-        iptables -A OUTPUT -o "${iface_name[${iface_idx}]}" -j ACCEPT
-    elif [[ "${iface_name[${iface_idx}]}" =~ ^int ]]
+        echo "iptables -A INPUT  -i "${iface_name[${zone_iface[${zone_name}]}]}" -j ACCEPT"
+        iptables -A INPUT  -i "${iface_name[${zone_iface[${zone_name}]}]}" -j ACCEPT
+        echo "iptables -A OUTPUT -o "${iface_name[${zone_iface[${zone_name}]}]}" -j ACCEPT"
+        iptables -A OUTPUT -o "${iface_name[${zone_iface[${zone_name}]}]}" -j ACCEPT
+    elif [[ "${iface_name[${zone_iface[${zone_name}]}]}" =~ ^wan ]]
     then
-        echo "iptables -A INPUT  -i "${iface_name[${iface_idx}]}"  -m state --state ESTABLISHED,RELATED -j ACCEPT"
-        iptables -A INPUT  -i "${iface_name[${iface_idx}]}"  -m state --state ESTABLISHED,RELATED -j ACCEPT
-        echo "iptables -A OUTPUT -o "${iface_name[${iface_idx}]}"  -j ACCEPT"
-        iptables -A OUTPUT -o "${iface_name[${iface_idx}]}"  -j ACCEPT
+        echo "iptables -A INPUT  -i "${iface_name[${zone_iface[${zone_name}]}]}"  -m state --state ESTABLISHED,RELATED -j ACCEPT"
+        iptables -A INPUT  -i "${iface_name[${zone_iface[${zone_name}]}]}"  -m state --state ESTABLISHED,RELATED -j ACCEPT
+        echo "iptables -A OUTPUT -o "${iface_name[${zone_iface[${zone_name}]}]}"  -j ACCEPT"
+        iptables -A OUTPUT -o "${iface_name[${zone_iface[${zone_name}]}]}"  -j ACCEPT
     fi
 done
 
@@ -113,53 +94,42 @@ for params in "${!acces_externe[@]}"
     param_port=`echo "${acces_externe[${params}]}" | cut -d"|" -f3`
     param_proto=`echo "${acces_externe[${params}]}" | cut -d"|" -f2`
     param_nom=`echo "${acces_externe[${params}]}" | cut -d"|" -f1`
-    echo "${param_nom} --> iptables -A INPUT  -i \"${int1_iface}\" -s \"${param_source}\" -p \"${param_proto}\" --dport \"${param_port}\" -j ACCEPT"
-    iptables -A INPUT  -i "${iface_name[${zone_iface["int1"]}]}" -s "${param_source}" -p "${param_proto}" --dport "${param_port}" -j ACCEPT
+    echo "${param_nom} --> iptables -A INPUT  -i \"${iface_name[${zone_iface["wan1"]}]}\" -s \"${param_source}\" -p \"${param_proto}\" --dport \"${param_port}\" -j ACCEPT"
+    iptables -A INPUT  -i "${iface_name[${zone_iface["wan1"]}]}" -s "${param_source}" -p "${param_proto}" --dport "${param_port}" -j ACCEPT
     done
 echo "###############################################"
 echo "########## AUTORISATION DES FORWARD ###########"
 for zone_name in "${!zone_iface[@]}"
 do
-    if [[ "$zone_name" =~ ^zone ]]
+    if [[ "$zone_name" =~ ^lan ]]
     then
-        echo "iptables -A FORWARD -i \"${zone_name}\" -o \"int1\"  -j ACCEPT"
-        iptables -A FORWARD -i ${zone_name} -o int1  -j ACCEPT
-        echo "iptables -A FORWARD -i \"int1\"  -o \"${zone_name}\" -m state --state NEW,ESTABLISHED,RELATED     -j ACCEPT"
-        iptables -A FORWARD -i int1  -o ${zone_name} -m state --state NEW,ESTABLISHED,RELATED     -j ACCEPT
+        echo "iptables -A FORWARD -i \"${iface_name[${zone_iface[${zone_name}]}]}\" -o \"${iface_name[${zone_iface["wan1"]}]}\" -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT"
+        iptables -A FORWARD -i "${iface_name[${zone_iface[${zone_name}]}]}" -o "${iface_name[${zone_iface["wan1"]}]}" -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+        echo "iptables -A FORWARD -i \"${iface_name[${zone_iface["wan1"]}]}\"  -o \"${iface_name[${zone_iface[${zone_name}]}]}\" -m state --state ESTABLISHED,RELATED     -j ACCEPT"
+        iptables -A FORWARD -i "${iface_name[${zone_iface["wan1"]}]}"  -o "${iface_name[${zone_iface[${zone_name}]}]}" -m state --state ESTABLISHED,RELATED     -j ACCEPT
     fi
-
 done
-## autorisation de forward int<-->zone1 pour les liens établies, cette zone est une zone de travail
-#iptables -A FORWARD -i "${zone1_iface}" -o "${int1_iface}"  -j ACCEPT
-#iptables -A FORWARD -i "${int1_iface}"  -o "${zone1_iface}" -m state --state NEW,ESTABLISHED,RELATED     -j ACCEPT
-
-## autorisation de forward int<-->zone2 pour les liens établies, cette zone est une dmz
-#iptables -A FORWARD -i "${zone2_iface}" -o "${int1_iface}"  -j ACCEPT
-#iptables -A FORWARD -i "${int1_iface}"  -o "${zone2_iface}" -m state --state NEW,ESTABLISHED,RELATED     -j ACCEPT
 
 for zone_name in "${!zone_iface[@]}"
 do
-    if [[ ! "$zone_name" =~ ^int ]] && [ ! "$zone_name" == "zone1" ]
+    if [[ ! "$zone_name" =~ ^wan ]] && [ ! "$zone_name" == "lan1" ]
     then
-        echo "iptables -A FORWARD -i \"zone1\" -o \"${zone_name}\" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT"
-        iptables -A FORWARD -i "zone1" -o "${zone_name}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-        echo "iptables -A FORWARD -i \"${zone_name}\" -o \"zone1\" -m state --state ESTABLISHED,RELATED -j ACCEPT"
-        iptables -A FORWARD -i "${zone_name}" -o "zone1" -m state --state ESTABLISHED,RELATED -j ACCEPT
+        echo "iptables -A FORWARD -i \"${iface_name[${zone_iface["lan1"]}]}\" -o \"${iface_name[${zone_iface[${zone_name}]}]}\" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT"
+        iptables -A FORWARD -i "${iface_name[${zone_iface["lan1"]}]}" -o "${iface_name[${zone_iface[${zone_name}]}]}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+        echo "iptables -A FORWARD -i \"${iface_name[${zone_iface[${zone_name}]}]}\" -o \"${iface_name[${zone_iface["lan1"]}]}\" -m state --state ESTABLISHED,RELATED -j ACCEPT"
+        iptables -A FORWARD -i "${iface_name[${zone_iface[${zone_name}]}]}" -o "${iface_name[${zone_iface["lan1"]}]}" -m state --state ESTABLISHED,RELATED -j ACCEPT
     fi
 done
 
-## autorisation de forward zone1-->zone2, pour les liens établies
-# tout ce qui vient de la zone1 en direction de la zone2 est accepter
-#iptables -A FORWARD -i "${zone1_iface}" -o "${zone2_iface}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-#iptables -A FORWARD -i "${zone2_iface}" -o "${zone1_iface}" -m state --state ESTABLISHED,RELATED -j ACCEPT
 echo "###############################################"
 echo
 ## Mise en place du masquerade
-# a l'exeption de tout ce qui est a destination de l'ip internet
-# ce qui permet un rebouclage sur les service interne avec un DNS externe
 echo "######### MISE EN PLACE DU MASQUERADE #########"
-iptables -t nat -A POSTROUTING -o "${int1_iface}" -s "${zone1_network}" -j MASQUERADE
-iptables -t nat -A POSTROUTING -o "${int1_iface}" -s "${zone2_network}" -j MASQUERADE
+echo "iptables -t nat -A POSTROUTING -o \"${iface_name[${zone_iface["wan1"]}]}\" -s \"${iface_network[${zone_iface["lan1"]}]}\" -j MASQUERADE"
+iptables -t nat -A POSTROUTING -o "${iface_name[${zone_iface["wan1"]}]}" -s "${iface_network[${zone_iface["lan1"]}]}" -j MASQUERADE
+echo "iptables -t nat -A POSTROUTING -o \"${iface_name[${zone_iface["wan1"]}]}\" -s \"${iface_network[${zone_iface["lan2"]}]}\" -j MASQUERADE"
+iptables -t nat -A POSTROUTING -o "${iface_name[${zone_iface["wan1"]}]}" -s "${iface_network[${zone_iface["lan2"]}]}" -j MASQUERADE
+
 echo "###############################################"
 echo
 # Mise en place du routage de port
@@ -172,16 +142,18 @@ for i in "${!port_routage[@]}"
     ip_destination=`echo "${port_routage[$i]}" | cut -d "|" -f4`
     port_destination=`echo "${port_routage[$i]}" | cut -d "|" -f5`
     echo "${service_name}"
-    echo "iptables -t nat -A PREROUTING -i \"${int1_iface}\" -p \"${proto}\" --dport \"${int1_port}\" -j DNAT --to-destination \"${ip_destination}:${port_destination}\""
-#       echo "iptables -t nat -A PREROUTING -d \"${int1_address}\" -p \"${proto}\" --dport \"${int1_port}\" -j DNAT --to-destination \"${ip_destination}:${port_destination}\""
-    iptables -t nat -A PREROUTING  -i "${int1_iface}" -p "${proto}" --dport "${int1_port}" -j DNAT --to-destination "${ip_destination}:${port_destination}"
+    echo "iptables -t nat -A PREROUTING -d \"${iface_address[${zone_iface["wan1"]}]}\" -p \"${proto}\" --dport \"${int1_port}\" -j DNAT --to-destination \"${ip_destination}:${port_destination}\""
+    iptables -t nat -A PREROUTING  -d "${iface_address[${zone_iface["wan1"]}]}" -p "${proto}" --dport "${int1_port}" -j DNAT --to-destination "${ip_destination}:${port_destination}"
+    # On autorise le forward pour les ports ouverts
+    echo "iptables -A FORWARD -i \"${iface_name[${zone_iface["wan1"]}]}\" -p \"${proto}\" --dport \"${int1_port}\" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT"
+    iptables -A FORWARD -i ${iface_name[${zone_iface["wan1"]}]} -p "${proto}" --dport "${int1_port}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
     done
 echo "###############################################"
 echo
 
 ## on bloque tout le reste
-iptables -A INPUT -i "${int1_iface}"  -j REJECT
-iptables -A INPUT -i "${zone2_iface}" -j REJECT
+iptables -A INPUT -i "${iface_name[${zone_iface["wan1"]}]}" -j REJECT
+iptables -A INPUT -i "${iface_name[${zone_iface["lan1"]}]}" -j REJECT
 
 iptables -nvL --line-number
 iptables -t nat -nvL --line-number
